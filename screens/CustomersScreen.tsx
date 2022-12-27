14b492f861a5671acdc25323f7d43aef4c72c383
David Bello <davidbello998@gmail.com>
2022-12-27 10:55:51 -0500

    Configured Stepzen

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,12 +1,12 @@
 import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { useTailwind } from 'tailwind-rn/dist';
 import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
 import { TabStackParamList } from '../navigator/TabNavigator';
 import { NativeStackNavigatorProps } from '@react-navigation/native-stack/lib/typescript/src/types';
 import { RootStackParamList } from '../navigator/RootNavigator';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs/lib/typescript/src/types';
-import { Image } from '@rneui/themed';
+import { Image, Input } from '@rneui/themed';
 
 export type CustomersScreenNavigationProp = CompositeNavigationProp<
 BottomTabNavigationProp<TabStackParamList, 'Customers'>, NativeStackNavigatorProps<RootStackParamList>
@@ -15,7 +15,7 @@ BottomTabNavigationProp<TabStackParamList, 'Customers'>, NativeStackNavigatorPro
 const CustomersScreen = () => {
   const tw = useTailwind();
   const navigation = useNavigation<CustomersScreenNavigationProp>();
-
+  const [input, setInput] = useState<string>()
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -26,6 +26,9 @@ const CustomersScreen = () => {
       <Image 
         source = {{uri: 'https://links.papareact.com/3jc'}}
         containerStyle ={tw("w-full h-64")}/>
+
+    <Input placeholder="Search by customer" onChangeText={setInput}
+      containerStyle={tw("bg-white pt-5 pb-0 px-10")}/>  
     </ScrollView>
   )
 }
