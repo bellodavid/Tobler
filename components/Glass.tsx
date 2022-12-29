7fd825e3dd8b5b2b0ea3c7823267db742e1031f3
David Bello <davidbello998@gmail.com>
2022-12-29 18:34:43 -0500

    Co-authored-by: davidbello998@gmail.com > Co-authored-by: bellodavid698@gmail.com

--- components/Glass.tsx
+++ components/Glass.tsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React, { useState } from 'react'
 
 const Glass = () => {
-  const [name, setName] =useState<string>("")
+  const [name, setName] =useState<string>("All")
   const [time, setTime] = useState<string>("")
   return (
     <View>
