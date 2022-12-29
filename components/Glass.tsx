a1666573847e5ed2e78e08897393476cbaedc87d
David Bello <davidbello998@gmail.com>
2022-12-29 18:57:20 -0500

    features Co-authored-by: bellodavid698@yahoo.com

--- components/Glass.tsx
+++ components/Glass.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 const Glass = () => {
   const [name, setName] =useState<string>("All")
   const [time, setTime] = useState<string>("")
+  //Working with master card
   return (
     <View>
       <Text>Glass</Text>
