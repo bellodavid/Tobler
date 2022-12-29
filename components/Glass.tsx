fa6052136a124425e5fd4070e880763c14d801a4
David Bello <davidbello998@gmail.com>
2022-12-29 18:20:04 -0500

    glass

--- components/Glass.tsx
+++ components/Glass.tsx
@@ -1,7 +1,8 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Glass = () => {
+  const [name, setName] =useState<string>("")
   return (
     <View>
       <Text>Glass</Text>
