8ed7dfa3d44e2eb63470b0ebf3b4dec51caba05f
David Bello <davidbello998@gmail.com>
2023-01-01 19:14:44 -0500

    updated Order component

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
 import { RootStackParamList } from '../navigator/RootNavigator';
 import { TabStackParamList } from '../navigator/TabNavigator';
-import { CompositeNavigationProp } from '@react-navigation/native';
+import { CompositeNavigationProp, RouteProp, useNavigation } from '@react-navigation/native';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useTailwind } from 'tailwind-rn/dist';
+import useOrders from '../hooks/useOrders';
+import { color } from '@rneui/base';
+import { Image } from '@rneui/themed';
+
+type OrderScreenRouteProp = RouteProp<RootStackParamList, "Order">;
 
 
 type OrderScreenNavigationProp = CompositeNavigationProp<
@@ -13,10 +19,30 @@ NativeStackNavigationProp<RootStackParamList, "Order">
 >;
 
 const OrdersScreen = () => {
+  const tw = useTailwind();
+  const navigation =useNavigation<OrderScreenNavigationProp>();
+  const { loading, error, orders } = useOrders();
+  const [ascending, setAscending] = useState<boolean>(false);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      tabBarLabel: ({ focused, color }) => (
+        <Text style = {{ color: focused ? "#5EB6A7C" : color, fontSize: 10}}>Orders</Text>
+      ),
+
+    });
+  },[]);
+
   return (
-    <View>
-      <Text>OrderScreen</Text>
-    </View>
+    <ScrollView>
+      <Image source={{uri: "https://links.papareact.com/m51"}}
+      containerStyle={tw("w-full h-64")}/>
+
+      <Button>
+        {ascending ? "Showing: Oldest First" : "Showing: Most Recent First"}
+      </Button>
+    </ScrollView>
   )
 }
 
